cd4c5b570612e8d74cd4f8452a8d2c98f9df2f56
Aman Nagarkar | Deep Learning <36772718+amannagarkar@users.noreply.github.com>
2023-07-19 11:44:05 -0700

    Updated links and removed dependencies.

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,18 +1,7 @@
 import React from 'react'
-import Masonry from '@mui/'
 
 function Portfolio() {
-  return (
-    <div>
-        <Masonry columns={4} spacing={2}>
-            {heights.map((height, index) => (
-                <Item key={index} sx={{ height }}>
-                {index + 1}
-                </Item>
-            ))}
-        </Masonry>
-    </div>
-  )
+  
 }
 
 export default Portfolio
